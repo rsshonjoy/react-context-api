948b1d8900a98c795031d6fbd52271f52a2f7165
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-06 23:52:26 +0600

    add increment button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <h1>This is a Home: </h1>
+      <h1>This is a Home: {count}</h1>
+      <button onClick={()=> setCount(count + 1)}>Increment</button>
     </div>
   );
 };
